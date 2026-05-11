b27bdb0ad298b077fe881b1663b605319ac0b570
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-05-11 16:30:49 -0500

    método de request a auth service cambiado a GET

--- src/infrastructure/http/AuthServiceClient.ts
+++ src/infrastructure/http/AuthServiceClient.ts
@@ -7,8 +7,7 @@ export class AuthServiceClient implements IAuthService {
 
     async validateUser(token: string): Promise<User> {
         try {
-            const response = await this.httpClient.post('/auth/me',
-                {},
+            const response = await this.httpClient.get('http://localhost:3000/auth/me',
                 {
                     headers: {
                         'Authorization': `Bearer ${token}`,
