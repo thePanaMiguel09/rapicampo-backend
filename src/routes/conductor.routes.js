8e5b425c428df25d5d200b5e4342bd3bb99446d6
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-05-11 11:00:56 -0500

    archivos de rutas eliminados

--- src/routes/conductor.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const ctrl   = require('../controllers/conductor.controller')
-const { authMiddleware, requireRole } = require('../middleware/auth.middleware')
-
-router.get('/cercanos',     ctrl.cercanos)
-router.put('/disponible',   authMiddleware, requireRole('conductor'), ctrl.setDisponible)
-router.put('/ubicacion',    authMiddleware, requireRole('conductor'), ctrl.setUbicacion)
-
-module.exports = router
--- src/routes/tracking.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const ctrl   = require('../controllers/tracking.controller')
-const { authMiddleware } = require('../middleware/auth.middleware')
-
-router.get('/:id',  authMiddleware, ctrl.historial)
-router.post('/:id', authMiddleware, ctrl.guardar)
-
-module.exports = router
