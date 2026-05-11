6170f0d6a0bbd2fc6d4ff882fdaf1416c1faf939
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-05-11 14:39:21 -0500

    implementación del router y configuración de index.js

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import express from 'express'
-import { Request, Response } from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from 'express';
+import cors from 'cors';
+import { config } from './infrastructure/config/env';
 import { createAuthServiceClient } from './infrastructure/api/axios.config';
 import { AuthServiceClient } from './infrastructure/http/AuthServiceClient';
 import { ValidateUserUseCase } from './application/use-cases/auth/ValidateUserUseCase';
 import { createAuthMiddleware } from './presentation/middleware/authMiddleware';
-import { ServiceController } from './presentation/controllers/ServiceController';
-import { CreateServiceUseCase } from './application/use-cases/services/CreateServiceUseCase';
-
+import { createServiceRouter } from './presentation/routes/services.routes';
+import { errorHandler, notFoundHandler } from './presentation/middleware/errorHandler';
+import dotenv from 'dotenv'
 dotenv.config();
 
 const app = express();
@@ -19,15 +18,19 @@ app.use(express.json());
 
 const httpCliente = createAuthServiceClient();
 const authServiceClient = new AuthServiceClient(httpCliente);
-
 const validateUserUseCase = new ValidateUserUseCase(authServiceClient);
-
 const authMiddleware = createAuthMiddleware(validateUserUseCase);
 
-app.get('/health', (_req: Request, res: Response) => {
+
+app.get('/health', (_req, res) => {
   res.json({ status: 'OK' });
 });
 
-app.listen(PORT, () => {
+app.use('/api/services', createServiceRouter(authMiddleware));
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+app.listen(config.PORT, () => {
   console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
 })
