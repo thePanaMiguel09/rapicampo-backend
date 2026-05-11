e9f3eae88949e845ea976fe493fddac06e4331c5
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-05-11 11:13:12 -0500

    cración del cliente para el servicio de autenticacion

---
+++ src/infrastructure/http/AuthServiceClient.ts
@@ -0,0 +1,37 @@
+import { AxiosInstance } from "axios";
+import { IAuthService } from "../../domain/ports/IAuthService";
+import { User } from "../../domain/entities/User";
+
+export class AuthServiceClient implements IAuthService {
+    constructor(private readonly httpClient: AxiosInstance) { }
+
+    async validateUser(token: string): Promise<User> {
+        try {
+            const response = await this.httpClient.post('http://localhost:3000/auth/me',
+                {},
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    }
+                }
+            );
+
+            return {
+                id: response.data.id,
+                nombre: response.data.nombre,
+                role: response.data.rol,
+                telefono: response.data.telefono,
+                modulo: response.data.modulo,
+                creado_en: response.data.creado_en
+            }
+
+        } catch (error: any) {
+            if (error.response?.status === 401) {
+                throw new Error('Token inválido o expirado');
+            }
+            throw new Error('Error al validar token con Auth Service');
+        }
+    }
+};
+
+
