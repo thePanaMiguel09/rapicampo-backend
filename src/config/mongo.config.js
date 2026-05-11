ec00bedac2e048a7f3a214eee341822a1fd6c0d9
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-05-11 10:59:29 -0500

    arhivos de configuración de mondo, postrgres y redis eliminados

--- src/config/mongo.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-async function connectMongo() {
-  await mongoose.connect(process.env.MONGO_URI || 'mongodb://mongodb:27017/rappicampo_tracking')
-  console.log('✅ MongoDB 8 conectado')
-}
-
-module.exports = { connectMongo }
