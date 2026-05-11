964502b85b5d5b5630eec640c16004d5baec4630
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-05-11 10:58:14 -0500

    archivos de utilidad y validacion eliminados

--- src/utils/bcrypt.util.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcryptjs')
-
-const hash    = (plain)        => bcrypt.hash(plain, 10)
-const compare = (plain, hashed) => bcrypt.compare(plain, hashed)
-
-module.exports = { hash, compare }
