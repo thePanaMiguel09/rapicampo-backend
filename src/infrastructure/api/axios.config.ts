c7cd5e1dea5d3776f819f9c1e7ce9324c9e054c6
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-05-11 11:12:49 -0500

    configuracion de instacia de axios para consumir servicios externos

---
+++ src/infrastructure/api/axios.config.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosInstance } from "axios";
+import { error } from "console";
+
+
+export const createAuthServiceClient = (): AxiosInstance => {
+    const client = axios.create({
+        baseURL: process.env.AUTH_SERVICE_URL || 'http://localhost:3000',
+        timeout: 5000,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    client.interceptors.request.use(
+        (config) => {
+            console.log(`[AuthService] ${config.method?.toUpperCase()} ${config.url}`);
+            return config;
+        },
+        (error) => Promise.reject(error)
+    );
+
+    client.interceptors.response.use(
+        (response) => response,
+        (error) => {
+            console.error('[AuthService] Error:', error.message);
+            return Promise.reject(error);
+        }
+    );
+
+    return client;
+}
