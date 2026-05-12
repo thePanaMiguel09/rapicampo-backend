4d661d5568593091e4bf53eee126ff5931d99be3
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-05-11 21:17:17 -0500

    updateService added to IServiceRepository

--- src/domain/ports/IServiceRepository.ts
+++ src/domain/ports/IServiceRepository.ts
@@ -4,4 +4,5 @@ export interface IServiceRepository {
     createService(service: Omit<Service, 'id'>): Promise<Service>;
     findServiceById(id: string): Promise<Service | null>;
     findAllServices(): Promise<Service[]>;
+    updateService(id: string, data: Partial<Omit<Service, 'id' | 'id_usuario_solicitante'>>): Promise<Service>;
 }
