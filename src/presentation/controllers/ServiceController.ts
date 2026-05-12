7ff6a3793faa95595c4f88901fa841267acf48a2
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-05-11 21:20:24 -0500

    controladores para obtener servicio, obtener todos los servicios y actualizar servicio añadidos a ServiceController

--- src/presentation/controllers/ServiceController.ts
+++ src/presentation/controllers/ServiceController.ts
@@ -1,9 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import { CreateServiceUseCase } from "../../application/use-cases/services/CreateServiceUseCase";
 import { successResponse } from "../interfaces/ApiResponse";
+import { FindServiceUseCase } from "../../application/use-cases/services/FindServiceUseCase";
+import { GetAllServicesUseCase } from "../../application/use-cases/services/GetAllServicesUseCase";
+import { UpdateServiceUseCase } from "../../application/use-cases/services/UpdateServiceUseCase";
 
 export class ServiceController {
-    constructor(private readonly createServiceUseCase: CreateServiceUseCase) { };
+    constructor(
+        private readonly createServiceUseCase: CreateServiceUseCase,
+        private readonly findServiceByIdUseCase: FindServiceUseCase,
+        private readonly fetchAllServicesUseCase: GetAllServicesUseCase,
+        private readonly updateServiceUseCase: UpdateServiceUseCase
+    ) { };
 
     async createService(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
@@ -15,6 +23,36 @@ export class ServiceController {
             const response = successResponse(service);
             res.status(201).json(response);
 
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async fetchServiceById(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const service = await this.findServiceByIdUseCase.execute(req.params.id);
+
+            res.status(200).json(successResponse(service));
+
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async fetchAllServices(_req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const services = await this.fetchAllServicesUseCase.execute();
+            res.status(200).json(successResponse(services));
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async updaService(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const { id } = req.params;
+            const updatedService = await this.updateServiceUseCase.execute(id, req.body);
+            res.status(200).json(successResponse(updatedService));
         } catch (error) {
             next(error);
         }
