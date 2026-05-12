733c766f97a81f0a25ec07b0118d93d730668167
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-05-11 21:21:48 -0500

    casos de uso encontrar servicio, obtener todos los servicios y actualizat servicio añadidos al ServiceController en services.routes

--- src/presentation/routes/services.routes.ts
+++ src/presentation/routes/services.routes.ts
@@ -4,6 +4,7 @@ import { CreateServiceUseCase } from "../../application/use-cases/services/Creat
 import { FindServiceUseCase } from "../../application/use-cases/services/FindServiceUseCase";
 import { GetAllServicesUseCase } from "../../application/use-cases/services/GetAllServicesUseCase";
 import { ServiceRepository } from "../../infrastructure/repositories/service.repository";
+import { UpdateServiceUseCase } from "../../application/use-cases/services/UpdateServiceUseCase";
 
 export const createServiceRouter = (authMiddleare: any): Router => {
     const router = Router();
@@ -12,14 +13,24 @@ export const createServiceRouter = (authMiddleare: any): Router => {
     const createServiceUseCase = new CreateServiceUseCase(serviceRepository);
     const findServiceUseCase = new FindServiceUseCase(serviceRepository);
     const getAllServicesUseCase = new GetAllServicesUseCase(serviceRepository);
+    const updateServiceUseCase = new UpdateServiceUseCase(serviceRepository);
 
     const controller = new ServiceController(
-        createServiceUseCase
+        createServiceUseCase,
+        findServiceUseCase,
+        getAllServicesUseCase,
+        updateServiceUseCase
     );
 
     router.use(authMiddleare);
 
     router.post('/', (req, res, next) => controller.createService(req, res, next));
+
+    router.get('/:id', (req, res, next) => controller.fetchServiceById(req, res, next));
+
+    router.get('/', (req, res, next) => controller.fetchAllServices(req, res, next));
+
+    router.put('/:id', (req, res, next) => controller.updaService(req, res, next));
 
     return router;
 }
