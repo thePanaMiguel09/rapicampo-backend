3e97953f6276d147c2ec3c9154d9242a24b5d5b9
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-05-13 18:00:35 -0500

    definición del caso de uso para aceptar un servicio - AcceptServiceUseCase

--- src/application/use-cases/services/AcceptServiceUseCase.ts
+++ src/application/use-cases/services/AcceptServiceUseCase.ts
@@ -1,10 +1,15 @@
+import { ServiceState } from "../../../domain/enums/service-state-enum";
 import { IServiceRepository } from "../../../domain/ports/IServiceRepository";
+import { INotificationService } from "../../../domain/ports/INotificationService";
 import { NotFoundError, ValidationError } from "../../../domain/errors";
 
 export class AcceptServiceUseCase {
-    constructor(private readonly serviceRepository: IServiceRepository){}
+    constructor(
+        private readonly serviceRepository: IServiceRepository,
+        private readonly notificationService: INotificationService
+    ) { }
 
-    async execute(serviceId: string, acceptingUserId: string): Promise<void> {
+    async execute(serviceId: string, acceptingUserId: string, acceptingUserName: string): Promise<void> {
         const service = await this.serviceRepository.findServiceById(serviceId);
 
         if (!service) {
@@ -13,10 +18,19 @@ export class AcceptServiceUseCase {
 
         if (acceptingUserId === service.requesterUserId) {
             throw new ValidationError('No puede aceptar tu propio servicio',
-                {servicio: ['No puedes aceptar tu propio servicio']}
+                { servicio: ['No puedes aceptar tu propio servicio'] }
             )
         }
 
-        
+        await this.serviceRepository.updateService(serviceId, {
+            serviceState: ServiceState.EN_PROCESO
+        });
+
+        await this.notificationService.notifyServiceAccepted(
+            serviceId,
+            service.requesterUserId,
+            acceptingUserName
+        )
+
     }
 }
