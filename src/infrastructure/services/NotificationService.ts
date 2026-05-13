7e3e8809bd8c1c62363be9bba6a257e17a7e19c5
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-05-13 18:02:58 -0500

    creación de servicio de notificaciones NotificationService implementando contrato INotificationService

---
+++ src/infrastructure/services/NotificationService.ts
@@ -0,0 +1,37 @@
+import { INotificationService } from "../../domain/ports/INotificationService";
+import { Notification } from "../../domain/entities/Notification";
+import { INotificationRepository } from "../../domain/ports/INotificationRepository";
+import { SocketServer } from "../websocket/SocketServer";
+import { NotificationType } from "../../domain/enums/notification-type-enum";
+
+export class NotificationService implements INotificationService {
+    constructor(
+        private readonly notificationRepository: INotificationRepository,
+        private readonly socketServer: SocketServer
+    ) { }
+
+    async sendNotification(notification: Notification): Promise<void> {
+        const savedNotification = await this.notificationRepository.create(notification);
+
+        this.socketServer.notifyUser(
+            notification.recipientUserId,
+            'new_notification',
+            savedNotification
+        );
+    }
+
+
+    async notifyServiceAccepted(serviceId: string, recipientUserId: string, acceptingUserName: string): Promise<void> {
+        const notification = new Notification(
+            recipientUserId,
+            NotificationType.SERVICE_ACCEPTED,
+            '¡Tu servicio fue aceptado!',
+            `${acceptingUserName} ha aceptado tu servicio`,
+            serviceId
+        );
+
+        await this.sendNotification(notification);
+    }
+
+
+}
