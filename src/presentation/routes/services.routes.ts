3455f4749259a8d55d7fadb48578dc80ea7982ff
Miguel Ángel Chávez <149839123+thePanaMiguel09@users.noreply.github.com>
2026-05-13 18:12:47 -0500

    enpoint para aceptar servicio añanido a services.routes

--- src/presentation/routes/services.routes.ts
+++ src/presentation/routes/services.routes.ts
@@ -5,21 +5,30 @@ import { FindServiceUseCase } from "../../application/use-cases/services/FindSer
 import { GetAllServicesUseCase } from "../../application/use-cases/services/GetAllServicesUseCase";
 import { ServiceRepository } from "../../infrastructure/repositories/service.repository";
 import { UpdateServiceUseCase } from "../../application/use-cases/services/UpdateServiceUseCase";
+import { AcceptServiceUseCase } from "../../application/use-cases/services/AcceptServiceUseCase";
+import { SocketServer } from "../../infrastructure/websocket/SocketServer";
+import { NotificationRepository } from "../../infrastructure/repositories/notification.repository";
+import { NotificationService } from "../../infrastructure/services/NotificationService";
 
-export const createServiceRouter = (authMiddleare: any): Router => {
+export const createServiceRouter = (authMiddleare: any, socketServer: any): Router => {
     const router = Router();
 
     const serviceRepository = new ServiceRepository();
+    const notificationRepository = new NotificationRepository();
+    const notificationService = new NotificationService(notificationRepository, socketServer);
+
     const createServiceUseCase = new CreateServiceUseCase(serviceRepository);
     const findServiceUseCase = new FindServiceUseCase(serviceRepository);
     const getAllServicesUseCase = new GetAllServicesUseCase(serviceRepository);
     const updateServiceUseCase = new UpdateServiceUseCase(serviceRepository);
+    const acceptServiceUseCase = new AcceptServiceUseCase(serviceRepository, notificationService);
 
     const controller = new ServiceController(
         createServiceUseCase,
         findServiceUseCase,
         getAllServicesUseCase,
-        updateServiceUseCase
+        updateServiceUseCase,
+        acceptServiceUseCase
     );
 
     router.use(authMiddleare);
@@ -31,6 +40,9 @@ export const createServiceRouter = (authMiddleare: any): Router => {
     router.get('/', (req, res, next) => controller.fetchAllServices(req, res, next));
 
     router.put('/:id', (req, res, next) => controller.updaService(req, res, next));
+
+    router.post('/:id/accept', (req, res, next) => controller.acceptService(req, res, next)); // Nueva ruta
+
 
     return router;
 }
